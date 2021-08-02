a460bf9245fbc055fe381612366f812c027e8fe8
Juan Miguel Guerrero <juanmiguel@existo.es>
2021-08-02 22:50:54 +0200

    test: on-demand builder function

--- functions/test/test.js
+++ functions/test/test.js
@@ -1,30 +1,21 @@
-const cmd = require('node-cmd');
+const { builder } = require('@netlify/functions');
 
+async function renderContent(event, context) {
 
-exports.handler = async (event, context) => {
-	
-	// event.path - The request path
-	// event.httpMethod - The request HTTP method
-	// event.headers - The request headers
-	// event.queryStringParameters - The request query parameters
-	// event.body - The request body in JSON format
-	// event.isBase64Encoded - A boolean flag to indicate if the applicable request payload is Base64-encode
-
-	console.log('Test function called...')
-
-	cmd.run(`cd functions
-	cd test
-	cd src
-	ls`, (err, data, stderr) => console.log("\n\n" + data))
+	let now = new Date();
+	console.log(`\n\n>> RenderContent function called... ${event.path}`)
 
 	return {
 
-		// isBase64Encoded: true|false,
-		// statusCode: httpStatusCode,
-		// headers: { "headerName": "headerValue", ... },
-		// body: "..."
-
 		statusCode: 200,
-		body: 'Hello from test lambda function !!!'
+		body: `
+		<html>
+			<body>
+				<h1>RenderContent function called... ${event.path}</h1>
+				<p>Time: ${now}</p>
+			</body>
+		</html>`
 	}
 }
+
+exports.handler = builder(renderContent);
